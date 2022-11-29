66e4fff1b87f39d1f248fada2f3695b8c738ee9c
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-29 20:16:25 +0100

    Options Pattern

--- ConfigurationExample/Controllers/HomeController.cs
+++ ConfigurationExample/Controllers/HomeController.cs
@@ -16,14 +16,15 @@ namespace ConfigurationExample.Controllers
 		[Route("/")]
 		public IActionResult Index()
 		{
-			// ViewBag.CliendID = _configuration["weatherapi"];
-			// ViewBag.ClientSecret = _configuration.GetValue("weatherapi:ClientSecret", "the default client secret");
+			// Bind: Loads configuration values into a new Options object
+			// WeatherApiOptions options = _configuration.GetSection("weatherapi").Get<WeatherApiOptions>();
 
-			IConfigurationSection weatherapiSection = _configuration.GetSection("weatherapi");
+			// Bind: Loads configuration values into existing Options object
+			WeatherApiOptions options = new WeatherApiOptions();
+			_configuration.GetSection("weatherapi").Bind(options);
 
-			ViewBag.CliendID = weatherapiSection["ClientID"];
-
-			ViewBag.ClientSecret = weatherapiSection["ClientSecret"];
+			ViewBag.ClientID = options.ClientID;
+			ViewBag.ClientSecret = options.ClientSecret;
 
 			return View();
 		}
